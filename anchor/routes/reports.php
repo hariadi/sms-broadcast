a324215a02d09e2ba193cfab1accc30e6fbae8ba
Hariadi Hinta <diperakui@yahoo.com>
2014-02-02 16:51:07 +0800

    update report form

--- anchor/routes/reports.php
+++ anchor/routes/reports.php
@@ -8,13 +8,101 @@ Route::collection(array('before' => 'auth,admin,csrf'), function() {
 	Route::get(array('admin/reports', 'admin/reports/(:num)'), function($page = 1) {
 
 		$vars['messages'] = Notify::read();
-		$vars['reports'] = Broadcast::paginate($page, Config::get('meta.posts_per_page'));
-		$vars['sorts'] = array('ID', 'Client', 'Message', 'Sender', 'Date', 'Status');
+		$vars['token'] = Csrf::token();
+		$vars['reports'] = Broadcast::paginate($page, Config::get('meta.posts_per_page'), Uri::to('admin/reports'));
 		$vars['status'] = 'all';
+
+		$vars['sorts'] = array(
+			'client' => __('global.client'),
+			'messages' => __('global.messages'),
+			'sender' => __('global.sender'),
+			'date' => __('global.date'),
+			'status' => __('global.status'),
+		);
+
+		$vars['types'] = array(
+			'month' => __('reports.month'),
+			'week' => __('reports.week'),
+			'day' => __('reports.day')
+		);
+
+		$vars['orders'] = array(
+			'month' => __('reports.asc'),
+			'week' => __('reports.desc')
+		);
 
 		return View::create('reports/index', $vars)
 			->partial('header', 'partials/header')
 			->partial('footer', 'partials/footer');
+	});
+
+	Route::get(array(
+		'admin/reports/interval', 
+		'admin/reports/interval/(:any)',
+		'admin/reports/interval/(:any)/(:num)'),
+
+	function($interval = 'month', $page = 1) {
+
+		$vars['messages'] = Notify::read();
+		$vars['token'] = Csrf::token();
+		$filter = array('use' => 'interval', 'interval' => $interval);
+		$vars['reports'] = Broadcast::search($filter, $page, Config::get('meta.posts_per_page'), Uri::to('admin/reports'));
+		print_r($vars['reports']);
+		exit();
+		$vars['status'] = 'all';
+
+		$vars['sorts'] = array(
+			'client' => __('global.client'),
+			'messages' => __('global.messages'),
+			'sender' => __('global.sender'),
+			'date' => __('global.date'),
+			'status' => __('global.status'),
+		);
+
+		$vars['types'] = array(
+			'month' => __('reports.month'),
+			'week' => __('reports.week'),
+			'day' => __('reports.day')
+		);
+
+		$vars['orders'] = array(
+			'month' => __('reports.asc'),
+			'week' => __('reports.desc')
+		);
+
+		return View::create('reports/index', $vars)
+			->partial('header', 'partials/header')
+			->partial('footer', 'partials/footer');
+	});
+
+	Route::post('admin/reports', function() {
+		$input = Input::get(array('use', 'interval', 'from_date', 'to_date', 'sort'));
+
+		if(!empty($input['from_date'])) {
+			$input['from_date'] = Date::mysql($input['from_date']);
+		}
+
+		if(!empty($input['to_date'])) {
+			$input['to_date'] = Date::mysql($input['to_date']);
+		}
+
+		if(empty($input['interval'])) {
+			$input['interval'] = 'month';
+		}
+
+		if(empty($input['sort'])) {
+			$input['sort'] = 'ID';
+		}
+
+		print_r($input);
+		exit();
+
+		return Response::redirect('admin/reports');
+	});
+
+	Route::get('admin/reports/export/(:any)', function($type) {
+
+		return Response::redirect('admin/reports');
 	});
 
 	/*
--- anchor/views/reports/index.php
+++ anchor/views/reports/index.php
@@ -1,100 +1,149 @@
 <?php echo $header; ?>
 
-<?php echo Html::link('admin/reports/sync', __('reports.sync_report'), array('class' => 'btn btn-lg btn-primary pull-right')); ?>
-
-
 <h1 class="page-header"><?php echo __('reports.report', 'Dashboard'); ?></h1>
 
 <?php echo $messages; ?>
 
-<form class="form-horizontal" method="post" action="<?php echo Uri::to('admin/report/update'); ?>" novalidate enctype="multipart/form-data" role="form">
+<form class="form-horizontal" method="post" action="<?php echo Uri::to('admin/reports'); ?>" novalidate enctype="multipart/form-data" role="form">
+  <input name="token" type="hidden" value="<?php echo $token; ?>">
 
-  <div class="form-group">
-    <label class="col-md-1 control-label" for="from_date"><?php echo __('reports.from_date'); ?></label>
+  <fieldset>
+    <legend>Search</legend>
 
-      <div class="input-group date col-sm-4">
-        <?php echo Form::text('from_date', Input::previous('from_date'), array(
-        'class' => 'form-control',
-        'id' => 'from_date',
-        )); ?>
-        <div class="input-group-btn">
-          <button class="btn btn-default" type="button"><span class="glyphicon glyphicon-calendar"></span></button>
+    <div class="form-group">
+      <label class="col-md-2 control-label" for="type"><?php echo __('reports.type'); ?></label>
+      <div class="row">
+        <div class="col-lg-1">
+          <input type="radio" name="use" value="interval" id="interval" checked="checked">
+        </div>
+        <div class="col-lg-7">
+          <div class="input-group col-md-4">
+            <?php echo Form::select('type', $types, Input::previous('type'), array(
+            'class' => 'form-control ',
+            'id' => 'type',
+            'onclick' => 'document.getElementById(\'interval\').checked = true',
+            'onfocus' => 'document.getElementById(\'interval\').checked = true',
+            )); ?>
+          </div>
+        </div>
+     </div>
+   </div>
+   <div class="form-group"><label class="col-md-2 control-label" for="from_date"><?php echo __('reports.from_date'); ?></label>
+    <div class="row">
+       <div class="col-lg-1">
+        <input type="radio" name="use" value="daterange" id="daterange">
+      </div>
+
+       <div class="col-lg-2">
+        <div class="input-group date">
+          <?php echo Form::text('from_date', Input::previous('from_date'), array(
+            'class' => 'form-control',
+            'id' => 'from_date',
+            'onclick' => 'document.getElementById(\'daterange\').checked = true',
+            'onfocus' => 'document.getElementById(\'daterange\').checked = true',
+            )); ?>
+          <div class="input-group-btn">
+            <button class="btn btn-default" type="button"><span class="glyphicon glyphicon-calendar"></span></button>
+          </div>
         </div>
       </div>
-  </div>
 
-<div class="form-group">
-  <label class="col-md-1 control-label" for="to_date"><?php echo __('reports.to_date'); ?></label>
+      <label class="col-md-2 control-label" for="to_date"><?php echo __('reports.to_date'); ?></label>
+      
+      <div class="col-lg-2">
+        <div class="input-group date">
+          <?php echo Form::text('from_date', Input::previous('from_date'), array(
+            'class' => 'form-control',
+            'id' => 'to_date',
+            'onclick' => 'document.getElementById(\'daterange\').checked = true',
+            'onfocus' => 'document.getElementById(\'daterange\').checked = true',
+            )); ?>
+            <div class="input-group-btn">
+              <button class="btn btn-default" type="button"><span class="glyphicon glyphicon-calendar"></span></button>
+            </div>
+          </div>
+        </div>
 
-    <div class="input-group date col-sm-4">
-      <?php echo Form::text('to_date', Input::previous('to_date'), array(
-      'class' => 'form-control',
-      'id' => 'to_date',
-      )); ?>
-      <div class="input-group-btn">
-        <button class="btn btn-default" type="button"><span class="glyphicon glyphicon-calendar"></span></button>
       </div>
     </div>
-</div>
-
-<div class="form-group">
-  <label class="col-lg-2 control-label" for="sort"><?php echo __('reports.sort'); ?></label>
-  <div class="col-lg-4">
-    <?php echo Form::select('sort', $sorts, Input::previous('sort'), array(
-      'class' => 'form-control ',
-      'id' => 'sort',
-    )); ?>
-  </div>
-</div>
-
-  <?php echo Form::button(__('global.submit'), array('type' => 'submit', 'class' => 'btn btn-primary')); ?>
-</form>       
 
 
-<h2 class="sub-header">Latest Activities</h2>
-<div class="table-responsive">
-    <table class="table table-striped">
-        <thead>
-        <tr>
-            <th>ID</th>
-            <th>Client</th>
-            <th>Message</th>
-            <th>Sender</th>
-            <th>Date</th>
-            <th>Status</th>
-        </tr>
-        </thead>
-        <tbody>
-        <?php if($reports->count): ?>
-        <?php foreach($reports->results as $report): ?>
-        <tr>
-          <!--td><a href="<?php echo Uri::to('admin/reports/view/')  . $report->id; ?>"><?php echo $report->id; ?></a></td-->
-          <td><?php echo $report->id; ?></td>
-          <td><?php echo $report->client_name; ?></td>
-          <td><?php echo $report->message; ?></td>
-          <td><?php echo $report->sender; ?></td>
-          <td><?php echo Date::format($report->date, 'jS F Y h:i A'); ?></td>
-          <td><span class="label label-<?php
-            $search  = array('success', 'pending', 'failed');
-            $replace = array('success', 'primary', 'warning');
-            echo str_replace($search, $replace, $report->status); 
-            ?>"><?php echo __('broadcasts.' . $report->status); ?></span></td>
-        </tr>
-        <?php endforeach; ?>
-        <?php else: ?>
-        <tr>
-          <td colspan="7"><?php echo __('reports.no_reports'); ?></td>
-        </tr>
-        <?php endif; ?>
+    <div class="form-group">
+      <label class="col-md-2 control-label" for="sort"><?php echo __('reports.sort'); ?></label>
+      <div class="row">
+        <div class="col-lg-2 col-md-offset-1">
+
+          <?php echo Form::select('sort', $sorts, Input::previous('sort'), array(
+            'class' => 'form-control ',
+            'id' => 'sort',
+            )); ?>
+
+        </div>
+        <label class="sr-only" for="order"><?php echo __('reports.order'); ?></label>
+        <div class="col-lg-2">
+          <?php echo Form::select('order', $orders, Input::previous('order'), array(
+            'class' => 'form-control ',
+            'id' => 'order',
+            )); ?></div>
+        </div>
+      
+        </div>
+
+              <div class="form-group">
+                <div class="col-md-offset-2">
+                  <?php echo Form::button(__('global.search'), array('type' => 'submit', 'class' => 'btn btn-primary')); ?>
+
+                  <?php echo Html::link('admin/posts/export/xls', __('global.export_xls'), array('class' => 'btn btn-success')); ?>
+                </div>
+              </div>
+            </fieldset>
+          </form>       
+
+
+          <h2 class="sub-header">Latest Activities</h2>
+          <div class="table-responsive">
+            <table class="table table-striped">
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Client</th>
+                  <th>Message</th>
+                  <th>Sender</th>
+                  <th>Date</th>
+                  <th>Status</th>
+                </tr>
+              </thead>
+              <tbody>
+                <?php if($reports->count): ?>
+                <?php foreach($reports->results as $report): ?>
+                <tr>
+                  <!--td><a href="<?php echo Uri::to('admin/reports/view/')  . $report->id; ?>"><?php echo $report->id; ?></a></td-->
+                  <td><?php echo $report->id; ?></td>
+                  <td><?php echo $report->client_name; ?></td>
+                  <td><?php echo $report->message; ?></td>
+                  <td><?php echo $report->sender; ?></td>
+                  <td><?php echo Date::format($report->created, 'jS F Y h:i A'); ?></td>
+                  <td><span class="label label-<?php
+                  $search  = array('success', 'pending', 'failed');
+                  $replace = array('success', 'primary', 'warning');
+                  echo str_replace($search, $replace, $report->status); 
+                  ?>"><?php echo __('broadcasts.' . $report->status); ?></span></td>
+                </tr>
+              <?php endforeach; ?>
+            <?php else: ?>
+            <tr>
+              <td colspan="7"><?php echo __('reports.no_reports'); ?></td>
+            </tr>
+          <?php endif; ?>
         </tbody>
-    </table>
+      </table>
 
-    <?php if ($reports->links()) : ?>
-        <ul class="pagination">
-            <?php echo $reports->links(); ?>
-        </ul>
-        <?php endif; ?>
+      <?php if ($reports->links()) : ?>
+      <ul class="pagination">
+        <?php echo $reports->links(); ?>
+      </ul>
+    <?php endif; ?>
 
-</div>
+  </div>
 
-<?php echo $footer; ?>
+  <?php echo $footer; ?>
