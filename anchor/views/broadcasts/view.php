acbf3aeb4ddea4722028c57560855c84f3b847ac
Hariadi Hinta <diperakui@yahoo.com>
2014-03-28 00:52:00 +0800

    prettification

--- anchor/views/broadcasts/view.php
+++ anchor/views/broadcasts/view.php
@@ -7,60 +7,55 @@
 
 <div class="row">
 	<div class="col col-lg-9">
+  	<div class="panel panel-default">
+      <div class="panel-heading"><?php echo __('broadcasts.broadcast_id', $broadcast->id); ?></div>
+  	  <!-- List group -->
+  	  <table class="table table-bordered">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th><?php echo __('global.status'); ?></th>
+              <td><span class="label label-<?php
+              $search  = array('success', 'pending', 'failed');
+              $replace = array('success', 'primary', 'warning');
+              echo str_replace($search, $replace, $broadcast->status); 
+              ?>"><?php echo __('broadcasts.' . $broadcast->status); ?></span></td>
+            </tr>
+            <tr>
+              <th><?php echo __('broadcasts.sender'); ?></th>
+              <td><?php echo $broadcast->sender; ?></td>
+            </tr>
+            <?php
+            $recipients = Json::decode($broadcast->recipient);
 
-	
-
-	<div class="panel panel-default">
-	  <div class="panel-heading"><?php echo __('broadcasts.broadcast_id', $broadcast->id); ?></div>
-	  <!-- List group -->
-	  <table class="table table-bordered">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Status</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th><?php echo __('global.status'); ?></th>
-            <td><span class="label label-<?php
-						$search  = array('success', 'pending', 'failed');
-						$replace = array('success', 'primary', 'warning');
-						echo str_replace($search, $replace, $broadcast->status); 
-						?>"><?php echo __('broadcasts.' . $broadcast->status); ?></span></td>
-          </tr>
-          <tr>
-            <th><?php echo __('broadcasts.sender'); ?></th>
-            <td><?php echo $broadcast->sender; ?></td>
-          </tr>
-          <?php
-					$recipients = Json::decode($broadcast->recipient);
-
-					if (!empty($recipients)) { 
-						$count = count($recipients);
-						$recipients = (is_array($recipients)) ? array_slice($recipients, 0, 5) : $recipients;
-					?>
-					<?php foreach($recipients as $key => $recipient): ?>
-          <tr>
-            <?php if($key === 0) :?><th rowspan="<?php echo $count; ?>"><?php echo __('broadcasts.recipient'); ?></th><?php endif?>
-            <td><code><?php echo $recipient ?></code></td>
-          	
-          </tr><?php endforeach; ?><?php } ?>
-          <tr>
-            <th><?php echo __('broadcasts.message'); ?></th>
-            <td><?php echo $broadcast->message; ?></td>
-          </tr>
-          <tr>
-            <th><?php echo __('global.created'); ?></th>
-            <td><?php echo Date::format($broadcast->created, 'jS F Y h:i A'); ?></td>
-          </tr>
-        </tbody>
-      </table>
-	</div>
+            if (!empty($recipients)) { 
+              $count = count($recipients);
+              $recipients = (is_array($recipients)) ? array_slice($recipients, 0, 5) : $recipients;
+            ?>
+  					<?php foreach($recipients as $key => $recipient): ?>
+            <tr>
+              <?php if($key === 0) :?><th rowspan="<?php echo $count; ?>"><?php echo __('broadcasts.recipient'); ?></th><?php endif?>
+              <td><code><?php echo $recipient ?></code></td>
+            	
+            </tr><?php endforeach; ?><?php } ?>
+            <tr>
+              <th><?php echo __('broadcasts.message'); ?></th>
+              <td><?php echo $broadcast->message; ?></td>
+            </tr>
+            <tr>
+              <th><?php echo __('global.created'); ?></th>
+              <td><?php echo Date::format($broadcast->created, 'jS F Y h:i A'); ?></td>
+            </tr>
+          </tbody>
+        </table>
+  	</div>
 
 			<?php if ($count > 5) :?><em>Display only 5 from <?php echo $count ?> numbers</em><?php endif; ?>
-
-
 	</div>
 </div>
 
--- anchor/views/users/add.php
+++ anchor/views/users/add.php
@@ -9,157 +9,157 @@
 <div class="row">
   <div class="col-lg-12">
 
-  	<form class="form-horizontal" method="post" action="<?php echo Uri::to('admin/users/add'); ?>" novalidate  autocomplete="off" enctype="multipart/form-data">
+    <form class="form-horizontal" method="post" action="<?php echo Uri::to('admin/users/add'); ?>" novalidate  autocomplete="off" enctype="multipart/form-data">
 
-		<input name="token" type="hidden" value="<?php echo $token; ?>">
+      <input name="token" type="hidden" value="<?php echo $token; ?>">
 
-			<fieldset class="col-lg-8">
+      <fieldset class="col-lg-8">
 
-				<div class="form-group">
-	        <label class="col-lg-2 control-label" for="real_name"><?php echo __('users.real_name'); ?></label>
-	        <div class="col-lg-10">
-	          <?php echo Form::text('real_name', Input::previous('real_name'), array(
-							'placeholder' => __('users.real_name_explain'),
-							'class' => 'form-control',
-							'id' => 'real_name',
-						)); ?>
-	        </div>
-	      </div>
+        <div class="form-group">
+          <label class="col-lg-2 control-label" for="real_name"><?php echo __('users.real_name'); ?></label>
+          <div class="col-lg-10">
+            <?php echo Form::text('real_name', Input::previous('real_name'), array(
+              'placeholder' => __('users.real_name_explain'),
+              'class' => 'form-control',
+              'id' => 'real_name',
+              )); ?>
+            </div>
+          </div>
 
-	      <div class="form-group">
-	        <label class="col-lg-2 control-label" for="bio"><?php echo __('users.bio'); ?></label>
-	        <div class="col-lg-10">
-	          <?php echo Form::textarea('bio', Input::previous('bio'), array(
-							'placeholder' => __('users.bio_explain'),
-							'rows' => 2,
-							'class' => 'form-control',
-							'id' => 'bio'
-						)); ?>
-	        </div>
-	      </div>
+          <div class="form-group">
+            <label class="col-lg-2 control-label" for="bio"><?php echo __('users.bio'); ?></label>
+            <div class="col-lg-10">
+              <?php echo Form::textarea('bio', Input::previous('bio'), array(
+                'placeholder' => __('users.bio_explain'),
+                'rows' => 2,
+                'class' => 'form-control',
+                'id' => 'bio'
+                )); ?>
+              </div>
+            </div>
 
-	      <div class="form-group">
-	        <label class="col-lg-2 control-label" for="status"><?php echo __('users.status'); ?></label>
-	        <div class="col-lg-4">
-	          <?php echo Form::select('status', $statuses, Input::previous('status', 'active'), array(
-							'class' => 'form-control ',
-							'id' => 'status',
-						)); ?>
-						<p class="help-block"><?php echo __('users.status_explain'); ?></p>
-	        </div>
-	      </div>
+            <div class="form-group">
+              <label class="col-lg-2 control-label" for="status"><?php echo __('users.status'); ?></label>
+              <div class="col-lg-4">
+                <?php echo Form::select('status', $statuses, Input::previous('status', 'active'), array(
+                  'class' => 'form-control ',
+                  'id' => 'status',
+                  )); ?>
+                  <p class="help-block"><?php echo __('users.status_explain'); ?></p>
+                </div>
+              </div>
 
-	      <div class="form-group">
-	        <label class="col-lg-2 control-label" for="role"><?php echo __('users.role'); ?></label>
-	        <div class="col-lg-4">
-	          <?php echo Form::select('role', $roles, Input::previous('role', 'client'), array(
-							'class' => 'form-control ',
-							'id' => 'role',
-						)); ?>
-						<p class="help-block"><?php echo __('users.role_explain'); ?></p>
-	        </div>
-	      </div>
+              <div class="form-group">
+                <label class="col-lg-2 control-label" for="role"><?php echo __('users.role'); ?></label>
+                <div class="col-lg-4">
+                  <?php echo Form::select('role', $roles, Input::previous('role', 'client'), array(
+                    'class' => 'form-control ',
+                    'id' => 'role',
+                    )); ?>
+                    <p class="help-block"><?php echo __('users.role_explain'); ?></p>
+                  </div>
+                </div>
 
-	      <div class="form-group">
-	        <label class="col-lg-2 control-label" for="credit"><?php echo __('users.credit'); ?></label>
-	        <div class="col-lg-5">
-	          <?php echo Form::text('credit', Input::previous('credit'), array(
-	          				'value' => 0,
-							'class' => 'form-control',
-							'id' => 'credit',
-						)); ?>
-						<p class="help-block"><?php echo __('users.credit_explain'); ?></p>
-	        </div>
-	      </div>
+                <div class="form-group">
+                  <label class="col-lg-2 control-label" for="credit"><?php echo __('users.credit'); ?></label>
+                  <div class="col-lg-5">
+                    <?php echo Form::text('credit', Input::previous('credit'), array(
+                      'value' => 0,
+                      'class' => 'form-control',
+                      'id' => 'credit',
+                      )); ?>
+                      <p class="help-block"><?php echo __('users.credit_explain'); ?></p>
+                    </div>
+                  </div>
 
-	      <div class="form-group">
-	        <label class="col-lg-2 control-label" for="charge"><?php echo __('users.charge'); ?></label>
-	        <div class="col-lg-5">
-	          <?php echo Form::text('charge', Input::previous('charge'), array(
-	          				'value' => 0,
-							'class' => 'form-control',
-							'id' => 'charge',
-						)); ?>
-						<p class="help-block"><?php echo __('users.charge_explain'); ?></p>
-	        </div>
-	      </div>
+                  <div class="form-group">
+                    <label class="col-lg-2 control-label" for="charge"><?php echo __('users.charge'); ?></label>
+                    <div class="col-lg-5">
+                      <?php echo Form::text('charge', Input::previous('charge'), array(
+                        'value' => 0,
+                        'class' => 'form-control',
+                        'id' => 'charge',
+                        )); ?>
+                        <p class="help-block"><?php echo __('users.charge_explain'); ?></p>
+                      </div>
+                    </div>
 
-			<?php
-	      	$expired = new DateTime(Date::mysql('now'));;
-	      	$expired->modify('+3 month');
-	      	?>
-	      <div class="form-group">
-	      		
+                    <?php
+                    $expired = new DateTime(Date::mysql('now'));;
+                    $expired->modify('+3 month');
+                    ?>
+                    <div class="form-group">
+                      
 
-		      <label class="col-md-2 control-label" for="expired"><?php echo __('users.credit_expired'); ?></label>
+                      <label class="col-md-2 control-label" for="expired"><?php echo __('users.credit_expired'); ?></label>
 
-		      <div class="col-lg-4">
-		        <div class="input-group date">
-		          <?php echo Form::text('expired', Input::previous('expired'), array(
-		            'value' => $expired->format('Y-m-d H:i:s'),
-		            'class' => 'form-control',
-		            'id' => 'expired',
-		            )); ?>
-		          <div class="input-group-btn">
-		            <button class="btn btn-default" type="button"><span class="glyphicon glyphicon-calendar"></span></button>
-		          </div>
-		        </div>
-		      </div>
-		    </div>
+                      <div class="col-lg-4">
+                        <div class="input-group date">
+                          <?php echo Form::text('expired', Input::previous('expired'), array(
+                            'value' => $expired->format('Y-m-d H:i:s'),
+                            'class' => 'form-control',
+                            'id' => 'expired',
+                            )); ?>
+                            <div class="input-group-btn">
+                              <button class="btn btn-default" type="button"><span class="glyphicon glyphicon-calendar"></span></button>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
 
-	      <div class="form-group">
-		      <div class="col-lg-10 col-lg-offset-2">
-		        <?php echo Form::button(__('global.create'), array('type' => 'submit', 'class' => 'btn btn-primary')); ?>
-		      </div>
-		    </div>
+                      <div class="form-group">
+                        <div class="col-lg-10 col-lg-offset-2">
+                          <?php echo Form::button(__('global.create'), array('type' => 'submit', 'class' => 'btn btn-primary')); ?>
+                        </div>
+                      </div>
 
-			</fieldset>
-			<fieldset class="col-lg-4">
+                    </fieldset>
+                    <fieldset class="col-lg-4">
 
-				<div class="form-group">
-	        <label class="col-lg-2 control-label" for="username"><?php echo __('users.username'); ?></label>
-	        <div class="col-lg-10">
-	          <?php echo Form::text('username', Input::previous('username'), array(
-							'placeholder' => __('users.username_explain'),
-							'class' => 'form-control',
-							'id' => 'username',
-						)); ?>
-	        </div>
-	      </div>
+                      <div class="form-group">
+                        <label class="col-lg-2 control-label" for="username"><?php echo __('users.username'); ?></label>
+                        <div class="col-lg-10">
+                          <?php echo Form::text('username', Input::previous('username'), array(
+                            'placeholder' => __('users.username_explain'),
+                            'class' => 'form-control',
+                            'id' => 'username',
+                            )); ?>
+                          </div>
+                        </div>
 
-	      <div class="form-group">
-	        <label class="col-lg-2 control-label" for="password"><?php echo __('users.password'); ?></label>
-	        <div class="col-lg-10">
-	          <?php echo Form::password('password', array(
-							'class' => 'form-control',
-							'id' => 'password',
-						)); ?>
-	        </div>
-	      </div>
+                        <div class="form-group">
+                          <label class="col-lg-2 control-label" for="password"><?php echo __('users.password'); ?></label>
+                          <div class="col-lg-10">
+                            <?php echo Form::password('password', array(
+                              'class' => 'form-control',
+                              'id' => 'password',
+                              )); ?>
+                            </div>
+                          </div>
 
-	      <div class="form-group">
-	        <label class="col-lg-2 control-label" for="email"><?php echo __('users.email'); ?></label>
-	        <div class="col-lg-10">
-	          <?php echo Form::email('email', Input::previous('email'), array(
-							'placeholder' => __('users.email_explain'),
-							'class' => 'form-control',
-							'id' => 'email',
-						)); ?>
-	        </div>
-	      </div>
+                          <div class="form-group">
+                            <label class="col-lg-2 control-label" for="email"><?php echo __('users.email'); ?></label>
+                            <div class="col-lg-10">
+                              <?php echo Form::email('email', Input::previous('email'), array(
+                                'placeholder' => __('users.email_explain'),
+                                'class' => 'form-control',
+                                'id' => 'email',
+                                )); ?>
+                              </div>
+                            </div>
 
-	      <?php foreach($fields as $field): ?>
-				<p>
-					<label for="extend_<?php echo $field->key; ?>"><?php echo $field->label; ?>:</label>
-					<?php echo Extend::html($field); ?>
-				</p>
-				<?php endforeach; ?>
+                            <?php foreach($fields as $field): ?>
+                              <p>
+                                <label for="extend_<?php echo $field->key; ?>"><?php echo $field->label; ?>:</label>
+                                <?php echo Extend::html($field); ?>
+                              </p>
+                            <?php endforeach; ?>
 
-			</fieldset>
+                          </fieldset>
 
-		</form>
-	</div>
-</div>
+                        </form>
+                      </div>
+                    </div>
 
 
-<?php echo $footer; ?>
+                    <?php echo $footer; ?>
